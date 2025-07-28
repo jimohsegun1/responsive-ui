dd534a746603fa964562537fb5a7b9d66bcccc48
jimohsegun1 <jimohsegunj@gmail.com>
2025-07-28 20:42:45 +0100

    improved the nav inclusion for responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,45 @@
+"use client"; 
+
 import Link from 'next/link';
+import React from 'react'; 
+
 const NavBar = () => {
     return (
-        <nav className="bg-gray-800 p-4">
-            <ul className="flex space-x-4">
+        <nav className="bg-gray-800 p-4 text-white">
+            <ul className="flex flex-wrap justify-center sm:justify-start space-x-4">
                 <li>
-                    <Link href="/" className="text-white hover:underline">
+                    <Link href="/" className="hover:underline text-sm sm:text-base">
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link href="/login" className="text-white hover:underline">
-                        login
+                    <Link href="/login" className="hover:underline text-sm sm:text-base">
+                        Login
                     </Link>
                 </li>
                 <li>
-                    <Link href="/forgot-password" className="text-white hover:underline">
-                        forgot password
+                    <Link href="/forgot-password" className="hover:underline text-sm sm:text-base">
+                        Forgot Password
                     </Link>
                 </li>
                 <li>
-                    <Link href="/mfa" className="text-white hover:underline">
-                        mfa
+                    <Link href="/mfa" className="hover:underline text-sm sm:text-base">
+                        MFA
                     </Link>
                 </li>
                 <li>
-                    <Link href="/password-reset-sent" className="text-white hover:underline">
-                        password reset sent
+                    <Link href="/password-reset-sent" className="hover:underline text-sm sm:text-base">
+                        Password Reset Sent
                     </Link>
                 </li>
                 <li>
-                    <Link href="/set-new-password" className="text-white hover:underline">
-                        set new password
+                    <Link href="/set-new-password" className="hover:underline text-sm sm:text-base">
+                        Set New Password
                     </Link>
                 </li>
             </ul>
         </nav>
     );
 };
+
 export default NavBar;
